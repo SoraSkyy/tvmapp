9251c8fb98cad2aac989fb08e5048370087e0e2d
SoraSkyy <viorama@gmail.com>
2017-02-06 23:05:31 +0800

    Added qr code function

---
+++ app/src/main/java/tembusu/tvm/tvmapp/ScanActivity.java
@@ -0,0 +1,100 @@
+package tembusu.tvm.tvmapp;
+
+import android.*;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+/**
+ * Created by YongJie on 6/2/2017.
+ */
+public class ScanActivity extends AppCompatActivity {
+    SurfaceView cameraView;
+    BarcodeDetector barcode;
+    CameraSource cameraSource = null;
+    SurfaceHolder holder;
+
+    @Override
+    public void onStop() {
+        if (cameraSource != null) {
+            cameraSource.stop();
+            cameraSource.release();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scanactivity);
+        cameraView = (SurfaceView) findViewById(R.id.cameraView);
+        cameraView.setZOrderMediaOverlay(true);
+        holder = cameraView.getHolder();
+        barcode = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        if(!barcode.isOperational()){
+            Toast.makeText(getApplicationContext(), "Sorry, Couldn't setup the detector", Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+        cameraSource = new CameraSource.Builder(this, barcode)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(24)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(1920,1024)
+                .build();
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    if(ContextCompat.checkSelfPermission(ScanActivity.this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                        cameraSource.start(cameraView.getHolder());
+                    }
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+        barcode.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes =  detections.getDetectedItems();
+                if(barcodes.size() > 0){
+                    Intent intent = new Intent();
+                    intent.putExtra("barcode", barcodes.valueAt(0));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
