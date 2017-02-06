91da85f84ae1a13aa51e0c87bbbf2669b0e83029
marvinchin <chinkunsong.marvin@gmail.com>
2017-02-07 00:06:50 +0800

    Fixed tag participant endpoint

--- app/src/main/java/tembusu/tvm/tvmapp/AddParticipantActivity.java
+++ app/src/main/java/tembusu/tvm/tvmapp/AddParticipantActivity.java
@@ -32,7 +32,6 @@ public class AddParticipantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_participant);
-        name_input = (EditText)findViewById(R.id.name_input);
         bib_input = (EditText)findViewById(R.id.bib_input);
         wave_input = (EditText)findViewById(R.id.wave_input);
         status_text = (TextView)findViewById(R.id.status);
@@ -43,12 +42,18 @@ public class AddParticipantActivity extends AppCompatActivity {
         r = new RequestHandler(this, new JsonHttpResponseHandler() {
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, org.json.JSONObject response) {
                 status_text.setTextColor(Color.GREEN);
-                status_text.setText("Successfully added " + name_input.getText());
+                status_text.setText("Successfully added " + bib_input.getText());
                 clearFields();
             }
 
             public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, java.lang.Throwable throwable, org.json.JSONObject errorResponse) {
-                String errString = errorResponse.toString();
+                String errString;
+                try {
+                    errString = errorResponse.toString();
+
+                } catch (Exception e) {
+                    errString = "No error code";
+                }
                 status_text.setTextColor(Color.RED);
                 status_text.setText("ERROR: Failed to add! - " + errString);
             }
@@ -77,13 +82,13 @@ public class AddParticipantActivity extends AppCompatActivity {
     }
 
     public void clearFields() {
-        name_input.getText().clear();
+
         bib_input.getText().clear();
-        wave_input.getText().clear();
+        //wave_input.getText().clear();
     }
 
     public void submit(View view) {
-        r.sendNewParticipantRequest(name_input.getText().toString(), bib_input.getText().toString(), wave_input.getText().toString());
+        r.sendNewParticipantRequest(bib_input.getText().toString(), wave_input.getText().toString());
         status_text.setTextColor(Color.BLACK);
         status_text.setText("Request pending...");
     }
